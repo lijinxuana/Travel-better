c349f50fdd6e2fa820cf02331f77d40d889f67b3
lijinxuana <837127335@qq.com>
2017-08-02 16:25:13 +0800

    修改休闲娱乐
    修改休闲娱乐

--- TravelBetter/src/pages/select/selectfun.ts
+++ TravelBetter/src/pages/select/selectfun.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-select',
   templateUrl: 'selectfun.html'
 })
 export class SelectfunPage {
-
-  constructor(public navCtrl: NavController) {
-
+  option:any;
+  constructor(public navCtrl: NavController, public navp:NavParams) {
+      this.option=navp.get("option");
   }
 
 }
