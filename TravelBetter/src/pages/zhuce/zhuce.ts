025f51a41549f1995d43ebf26e319ac54a16d007
lijinxuana <837127335@qq.com>
2017-09-07 20:23:57 +0800

    zhuce
    zhuce

--- TravelBetter/src/pages/zhuce/zhuce.ts
+++ TravelBetter/src/pages/zhuce/zhuce.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+/*import { ZhuceProvider} from'../../providers/zhuce-provider';*/
 
 @Component({
   selector: 'page-zhuce',
@@ -12,31 +13,21 @@ export class zhucePage {
   mypass1:string;
   mypass2:string;
   datas;
+  password:string;
+  email:string;
 
   constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
 
   zhuce(){
-    this.datas=[this.myemail,this.mypass1,this.mypass2];
     if(this.mypass1==this.mypass2){
-        http.get("http://123.206.116.70:8080/hours/user_register?phone="+$scope.phone+"&password="+hex_md5($scope.password))
-      .then(function (response) {
-        if(response.data.status==0)
-        {
-           this.navCtrl.push(HomePage,{
+      let password=this.mypass1;
+      let email=this.myemail;
+    this.datas=[this.email,this.password];
+    /*this.navCtrl.push(ZhuceProvider,{
         datas:this.datas
-    })
-        }else
-        {
-    let alert = this.alertCtrl.create({
-      title: '注册错误',
-      subTitle: '您的注册出错，请重新注册',
-      buttons: ['我知道了']
-    });
-    alert.present();
-        }
-      });
-    }
+    })*/
+  }
   }
 }
