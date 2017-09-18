4222daf7728a17c5ff4ef9309477c31ebd85e333
lijinxuana <837127335@qq.com>
2017-09-18 15:15:14 +0800

    交互
    交互

--- TravelBetter/src/pages/service/sefoodservice.ts
+++ TravelBetter/src/pages/service/sefoodservice.ts
@@ -9,31 +9,31 @@ export class SefoodService{
   baseUrl: String;
   constructor(http:Http,public navCtrl:NavController,public params:NavParams){
     this.http = http;
-    this.baseUrl = 'http://123.206.22.15/food.json';
+    this.baseUrl = 'http://123.206.22.15/food.json/food?category=';
   }
 
   getPost1(){
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl+"6")
     .map(res => res.json());
   }
   getPost2(){
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl+"2")
     .map(res => res.json());
   }
   getPost3(){
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl+"3")
     .map(res => res.json());
   }
   getPost4(){
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl+"5")
     .map(res => res.json());
   }
   getPost5(){
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl+"4")
     .map(res => res.json());
   }
   getPost6(){
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl+"1")
     .map(res => res.json());
   }
 }
